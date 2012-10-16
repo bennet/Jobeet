55014024b120b48f7b5b83c6215db27aff4fb267
Bennet <bennet@comogroupap.com>
2012-10-16 12:59:55 +0530

    Update apps/frontend/modules/job/templates/_admin.php

--- apps/frontend/modules/job/templates/_admin.php
+++ apps/frontend/modules/job/templates/_admin.php
@@ -15,7 +15,7 @@
         <?php endif ?>
  
         <?php if ($job->expiresSoon()): ?>
-         - <a href="<?php echo url_for('job_extend', $job)?>">Extend</a> for another <?php echo sfConfig::get('app_active_days') ?> days
+         - <?php echo link_to('Extend', 'job_extend', $job, array('method' => 'put')) ?> for another <?php echo sfConfig::get('app_active_days') ?> days
         <?php endif ?>
       </li>
     <?php else: ?>
