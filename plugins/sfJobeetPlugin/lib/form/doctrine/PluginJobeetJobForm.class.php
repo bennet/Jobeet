222bd1508492a450ccc47fac37c347aa77706283
Bennet <bennet@comogroupap.com>
2012-10-19 09:08:50 +0530

    restore soft deleted validator call

--- plugins/sfJobeetPlugin/lib/form/doctrine/PluginJobeetJobForm.class.php
+++ plugins/sfJobeetPlugin/lib/form/doctrine/PluginJobeetJobForm.class.php
@@ -48,6 +48,10 @@ abstract class PluginJobeetJobForm extends BaseJobeetJobForm
             ));
       $this->widgetSchema->setHelp('is_public', 'Whether the job can also be published on affiliate websites or not.');
       $this->widgetSchema->setNameFormat('job[%s]');
+      
+      $this->validatorSchema->setPostValidator(
+            new sfValidatorDoctrineUniqueSoftDeleted(array('model' => 'JobeetJob', 'column' => array('token')))
+        );
   }
   
   protected function removeFields()
