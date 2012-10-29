751553c04cf065853fad518e4bf1e70c0c99ce70
Bennet <bennet@comogroupap.com>
2012-10-29 12:32:15 +0530

    batch action error with jroller corrected

--- apps/backend/modules/affiliate/actions/actions.class.php
+++ apps/backend/modules/affiliate/actions/actions.class.php
@@ -79,4 +79,50 @@ EOF
  
     $this->redirect('jobeet_affiliate');
   }
+  
+   public function executeBatch(sfWebRequest $request)
+  {
+    $request->checkCSRFProtection();
+
+    if (!$ids = $request->getParameter('ids'))
+    {
+      $this->getUser()->setFlash('error', 'You must at least select one item.');
+
+      $this->redirect('@jobeet_affiliate');
+    }
+
+    if (!$action = $request->getParameter('batch_action'))
+    {
+      $this->getUser()->setFlash('error', 'You must select an action to execute on the selected items.');
+
+      $this->redirect('@jobeet_affiliate');
+    }
+
+    if (!method_exists($this, $method = 'execute'.ucfirst($action)))
+    {
+      throw new InvalidArgumentException(sprintf('You must create a "%s" method for action "%s"', $method, $action));
+    }
+
+    if (!$this->getUser()->hasCredential($this->configuration->getCredentials($action)))
+    {
+      $this->forward(sfConfig::get('sf_secure_module'), sfConfig::get('sf_secure_action'));
+    }
+
+    $validator = new sfValidatorDoctrineChoice(array('model' => 'JobeetAffiliate'));
+    try
+    {
+      // validate ids
+//      $ids = $validator->clean($ids);
+
+      // execute batch
+      $this->$method($request);
+    }
+    catch (sfValidatorError $e)
+    {
+      $this->getUser()->setFlash('error', 'A problem occurs when deleting the selected items as some items do not exist anymore.');
+    }
+
+    $this->redirect('@jobeet_affiliate');
+  }
+
 }
